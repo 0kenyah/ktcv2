5c4f1264e042bec5e5f4500b81a43629753a4cf4
0kenyah <abzz2024xd@gmail.com>
2026-01-27 02:00:40 -0300

    Update vxnityhubpremium.lua

--- vxnityhubpremium.lua
+++ vxnityhubpremium.lua
@@ -297,7 +297,33 @@ local StarterGui = game:GetService("StarterGui")
 local Workspace = game:GetService("Workspace")
 
 local gameUpdated = false
+-- test --
+for i, v in getgc() do
+    if typeof(v) == "function" and islclosure(v) then
+        local c = debug.getconstants(v)
+        if table.find(c, "Asset Editing") or table.find(c, 23) and table.find(c, 50) and table.find(c, 65) then
+            for x = 1, #c do
+                if c[x] == 23 then
+                    debug.setconstant(v, x, 9e9)
+                elseif c[x] == 50 then
+                    debug.setconstant(v, x, 9e9)
+                elseif c[x] == 65 then
+                    debug.setconstant(v, x, 9e9)
+                end
+            end
+        end
+    end
+end
 
+local r = game:GetService("Workspace").FE.Actions:WaitForChild("KeepYourHeadUp")
+local o
+o = hookmetamethod(game, "__namecall", function(s, ...)
+    if s == r and getnamecallmethod() == "FireServer" and not checkcaller() then
+        return
+    end
+    return o(s, ...)
+	end
+	
 updateProgress(0.2, "Verificando Anticheat...")
 local function checkGameUpdate()
 	local success, result = pcall(function()
